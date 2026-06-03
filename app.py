06831cc69af6fa45c40b8e29b4be325d5cf50af7
mohini ghuge <mohinighuge49@gmail.com>
2026-06-03 16:26:04 +0530

    Day 5

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 stud = [
-    {"name":"Rohit","roll_no":1,"marks":85},
-    {"name":"Priya","roll_no":2,"marks":90},
-    {"name":"Amit","roll_no":3,"marks":78},
-    {"name":"Sneha","roll_no":4,"marks":92},
-    {"name":"Rahul","roll_no":5,"marks":88}
+    {"name":"Rohit  ","roll_no":1,"marks":85},
+    {"name":"Priya  ","roll_no":2,"marks":90},
+    {"name":"Amit   ","roll_no":3,"marks":78},
+    {"name":"Sneha  ","roll_no":4,"marks":92},
+    {"name":"Rahul  ","roll_no":5,"marks":88}
   ]
+notices_list= [
+    {
+        "title": "python training",
+        "date": "3 june 2026",
+        "message": "python internship started from 28th may."
+    }
+]
 
 @app.route('/')
 
 def home():
-  html = '<h1>Welcome to the College Smart Portal!</h1>'
-
-  html += '<h2>List of Students:</h2>'
-  for student in stud:
-     html +=f"<li>{student['name']} (Roll No: {student['roll_no']}, Marks: {student['marks']})</li>"
-  html +='</ul>'
-
-  return html
+    return render_template('home.html', students=stud)
 
 @app.route('/college_info')
 
 def college_info():
-        return '<h1>College Information</h1>'
+    return render_template('college_info.html', students=stud)
 
 @app.route('/students')
 
 def students():
-    return '<h1>List of Students</h1>'
+    return render_template('students.html', students=stud)
+
+@app.route('/notices')
+
+def notices():
+    return render_template('notices.html', notices=notices_list)
 
 if __name__ == '__main__':
+  
+   print("Welcome to College Smart Portal")
    app.run(debug=True)
 
